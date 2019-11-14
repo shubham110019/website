d07057f9f5f3d0b587be9b16d4f6261ab6a3acaf
shubham <sralli73@gmail.com>
2019-11-14 04:23:19 -0800

    website

---
+++ include/sidebar.php
@@ -0,0 +1,39 @@
+<div class="side-menu-fixed">
+     <div class="scrollbar side-menu-bg">
+      <ul class="nav navbar-nav side-menu" id="sidebarnav">
+      
+     
+         <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title">Components </li>
+        <!-- menu item Elements-->
+		
+		 <li>
+          <a href="home.php"><i class="ti-home"></i><span class="right-nav-text">Dashboard</span> </a>
+        </li>
+        
+		
+        <li>
+          <a href="javascript:void(0);" data-toggle="collapse" data-target="#elements">
+            <div class="pull-left"><i class="ti-palette"></i><span class="right-nav-text">Website</span></div>
+            <div class="pull-right"><i class="ti-plus"></i></div><div class="clearfix"></div>
+          </a>
+          <ul id="elements" class="collapse" data-parent="#sidebarnav">
+            <li><a href="website-home.php">Home</a></li>
+            <li><a href="website-about.php">About</a></li>
+            <li><a href="button.html">Button</a></li>
+           
+          </ul>
+        </li>
+		
+		 <li>
+          <a href="home.php"><i class="ti-palette"></i><span class="right-nav-text">Website</span></a>
+        </li>
+      
+    
+       
+        
+
+       
+      
+    </ul>
+  </div> 
+</div> 
